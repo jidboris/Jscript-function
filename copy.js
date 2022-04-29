2c9071fae7a4141272dea0392684a179ac6cff3e
Olajide Borisade <102518345+jidboris@users.noreply.github.com>
2022-04-29 23:16:54 +0100

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,13 @@
+var number = [1,2,3,4,5,6,7,8,9]
+var printSum = 0;
+for (var i = 0; i<number.length; i++)(printSum += number[i])
+function printSum() {
+    return printSum; 
+}
+
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let sum = 0;
+for (let i = 0; i < number.length; i++)(sum += number[i])
+function printSum(a) {
+    return a;
+}
